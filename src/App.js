ba6ad0237e64b46105db0251c168d9c4824c2fb7
NandhithaShree <142162325+NandhithaShree@users.noreply.github.com>
2024-07-25 00:33:14 +0800

    am able top change to nttk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import ButtonFn from './components/buttons';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Nttkfn from './normal_tiktaktoe';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <Routes>
+          <Route path="/" element={<ButtonFn />} /> {/*This route will render the ButtonFn component when the user navigates to the root URL ("/"). */}
+          <Route relative path="/normal_tiktaktoe" element={<Nttkfn />} />
+        </Routes>
+    </>
+  )
 }
 
 export default App;
