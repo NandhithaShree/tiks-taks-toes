eae1f0e92ccaa70f7f51e4d546a95a42f60e4486
NandhithaShree <142162325+NandhithaShree@users.noreply.github.com>
2024-08-18 13:05:20 +0800

    aa

--- src/components/3by3grid.js
+++ src/components/3by3grid.js
@@ -3,13 +3,85 @@ import "./3by3grid.css"
 import Xfn from './thex';
 import { useState } from 'react';
 import { smallTurn } from './thex';
-
+import { Helmet } from 'react-helmet';
 
 
 export default function Threebythreefn() {
+    let winner = null;
+    let list = [3, 4, 5, 6, 7, 8, 9, 10, 11];
+    const newList = [3, 4, 5, 6, 7, 8, 9, 10, 11];
+    const [isVisible, setIsVisible] = useState(false);
+    const Overlay = ({ OisVisible, Owinner }) => {
+        if (!isVisible) return null;
+
+        const overlayStyle = {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(71, 11, 43, 0.8)',
+            zIndex: 9999,
+        };
+
+        const textStyle = {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+        }
+
+        if (Owinner == 1) {
+            winner = "Player 2"
+        }
+        else {
+            winner = "Player 1"
+        }
+        return <div style={overlayStyle}>
+            <text className='wintext'>{winner} won!</text>
+            <Reloadbutfn/>
+        </div>;
+    };
+    function MyButton({ id }) {
+        const [content, setContent] = useState('');
+        function handleClick() {
+            if ((winner != null) || (list[id - 1] === -1 || list[id - 1] === 1)) return;
+            const result = Xfn();
+            setContent(result);
+            list[id - 1] = smallTurn;
+            console.log(id + "=" + smallTurn);
+            if (
+                (list[0] === list[1] && list[1] === list[2]) || // Horizontal row 1
+                (list[3] === list[4] && list[4] === list[5]) || // Horizontal row 2
+                (list[6] === list[7] && list[7] === list[8]) || // Horizontal row 3
+                (list[0] === list[3] && list[3] === list[6]) || // Vertical column 1
+                (list[1] === list[4] && list[4] === list[7]) || // Vertical column 2
+                (list[2] === list[5] && list[5] === list[8]) || // Vertical column 3
+                (list[2] === list[4] && list[4] === list[6]) || // Diagonal from top-right to bottom-left
+                (list[0] === list[4] && list[4] === list[8])    // Diagonal from top-left to bottom-right
+            ) {
+                winner = smallTurn;
+                console.log("theres a winner");
+                list = [...newList];
+                setIsVisible(true);
+            }
+            console.log(list[0], list[1], list[2], list[3], list[4], list[5], list[6], list[7], list[8])
+        }
+
+        return (
+            <button class="but-to-xfn" onClick={handleClick}>
+                {content}
+            </button>
+        );
+    }
     return (
         <div>
+            <Overlay OisVisible={isVisible} Owinner={winner}></Overlay>
             <div id="grid-parent">
+                <Helmet>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
+                </Helmet>
                 <div className="row">
                     <div className='kid right bottom'><MyButton id="1" /></div>
                     <div className='kid left right bottom'><MyButton id="2" /></div>
@@ -30,71 +102,22 @@ export default function Threebythreefn() {
 
     );
 }
-let winner = null;
-let list=[3,4,5,6,7,8,9,10,11];
-const newList = [3,4,5,6,7,8,9,10,11];
-function MyButton({id}) {
-    //const [isVisible, setIsVisible] = useState(false);
-    const [content, setContent] = useState('');
-    function handleClick() {
-        if ((winner!=null)|| (list[id-1]===-1||list[id-1]===1)) return;
-        const result = Xfn();
-        setContent(result);
-        list[id - 1] = smallTurn; 
-        console.log(id + "=" + smallTurn);
-        if (
-            (list[0] === list[1] && list[1] === list[2]) || // Horizontal row 1
-            (list[3] === list[4] && list[4] === list[5]) || // Horizontal row 2
-            (list[6] === list[7] && list[7] === list[8]) || // Horizontal row 3
-            (list[0] === list[3] && list[3] === list[6]) || // Vertical column 1
-            (list[1] === list[4] && list[4] === list[7]) || // Vertical column 2
-            (list[2] === list[5] && list[5] === list[8]) || // Vertical column 3
-            (list[2] === list[4] && list[4] === list[6]) || // Diagonal from top-right to bottom-left
-            (list[0] === list[4] && list[4] === list[8])    // Diagonal from top-left to bottom-right
-        ) {
-            winner=smallTurn;
-            console.log("theres a winner");
-            list=[...newList];
-        }
-      console.log(list[0],list[1],list[2],list[3],list[4],list[5],list[6],list[7],list[8])
-    }
 
-    return (
-        <button class="but-to-xfn" onClick={handleClick}>
-            {content}
-        </button>
-    );
+export function Reloadbutfn(){
+    return(
+        <div>
+            <button id="reload-but" onClick={() => window.location.reload()}>
+                RETRY
+            </button>
+            <button id="gohome-but" onClick={() => window.history.back()}>
+                HOME
+            </button>
+        </div>
+    )
 }
 
-    const Overlay = ({ isVisible,winner }) => {
-        if (!isVisible) return null;
-    
-        const overlayStyle = {
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            backgroundColor: 'rgba(255, 255, 255, 0.5)',
-            zIndex: 9999,
-        };
-    
-        const textStyle = {
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-        }
-    
-        if (winner) {
-            winner = "X"
-        }
-        else {
-            winner = "O"
-        }
-        return <div style={overlayStyle}>
-            <text style={textStyle}>{winner} won!!</text>
-        </div>;
-    };
+
+
 
 
 
